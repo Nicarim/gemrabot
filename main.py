46d7e1c660b9482203061c3302ba387b0f83fd92
Marcin Gordziejewski <655590+Nicarim@users.noreply.github.com>
2020-08-18 16:55:34 +0200

    Add really basic diff getting

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import logging
+import os
 
 import uvicorn
 from fastapi import FastAPI, Request
 
 from sources.gitlab import GitlabWebhook
 
-WEBHOOK_URL = "NO"
+SLACK_WEBHOOK_URL = os.getenv("SLACK_WEBHOOK_URL")
+GITLAB_API_KEY = os.getenv("GITLAB_API_KEY")
 app = FastAPI()
 
 logger = logging.getLogger("app")
@@ -13,7 +15,7 @@ logger = logging.getLogger("app")
 
 @app.post('/webhooks/gitlab')
 async def webhooks_gitlab(request: Request):
-    webhook = GitlabWebhook(request)
+    webhook = GitlabWebhook(request, GITLAB_API_KEY)
     is_valid = await webhook.validate()
     if not is_valid:
         logger.info("Dropping on x_gitlab_event")
--- sources/gitlab.py
+++ sources/gitlab.py
@@ -1,7 +1,11 @@
 import json
 import logging
 
+from aioify import aioify
 from fastapi import Request
+from gitlab import Gitlab
+from gitlab.v4.objects import Project
+from unidiff import PatchSet
 
 from structures import PullRequest, PullRequestStatus
 
@@ -9,8 +13,10 @@ logger = logging.getLogger(__name__)
 
 
 class GitlabWebhook:
-    def __init__(self, request: Request):
+    def __init__(self, request: Request, gitlab_access_key: str):
         self.request = request
+        self.api_key = gitlab_access_key
+        self.client = Gitlab('https://gitlab.com', private_token=self.api_key)
 
     async def validate(self) -> bool:
         x_gitlab_event = self.request.headers.get('X-Gitlab-Event', 'Invalid')
@@ -18,6 +24,22 @@ class GitlabWebhook:
             logger.error(f"Expected MR hook, got instead {x_gitlab_event}")
             return True
         return True
+
+    async def get_user_by_id(self, user_id: int):
+        gl_get_users = aioify(obj=self.client.users.get)
+        return await gl_get_users(user_id)
+
+    async def get_project_by_id(self, project_id: int):
+        gl_get_project = aioify(obj=self.client.projects.get)
+        return await gl_get_project(project_id)
+
+    async def get_project_merge_request_by_iid(self, project: Project, merge_request_id: int):
+        gl_get_mr = aioify(obj=project.mergerequests.get)
+        return await gl_get_mr(merge_request_id)
+
+    async def get_merge_request_changes(self, mr):
+        gl_get_changes = aioify(obj=mr.changes)
+        return await gl_get_changes()
 
     @staticmethod
     def get_mr_status(data) -> PullRequestStatus:
@@ -33,18 +55,35 @@ class GitlabWebhook:
         return json.loads(f.read())
         # return await self.request.json()
 
+    def build_patch_set(self, changes):
+        all_changes = ""
+        for change in changes['changes']:
+            changes_txt: str = change['diff']
+            header = f"diff --git a/{change['old_path']} b/{change['new_path']}\n"
+            header += f"index fixme...fixme 100644\n"
+            header += f"--- a/{change['old_path']}\n"
+            header += f"+++ b/{change['new_path']}\n"
+            all_changes += header
+            all_changes += changes_txt
+
+        patch = PatchSet(all_changes)
+        return patch
+
     async def parse(self) -> PullRequest:
         data = await self.get_data()
+        user = await self.get_user_by_id(data['object_attributes']['author_id'])
+        project = await self.get_project_by_id(data['object_attributes']['target_project_id'])
+        merge_request = await self.get_project_merge_request_by_iid(project, data['object_attributes']['iid'])
+        changes = await self.get_merge_request_changes(merge_request)
+        patch_set = self.build_patch_set(changes)
+
         pr = PullRequest(
             id=data['object_attributes']['iid'],  # Preferring internal ID as this will be used as reference
             title=data['object_attributes']['title'],
             description=data['object_attributes']['description'],
             status=self.get_mr_status(data),
-            # This may be incorrect as this suggest caller of webhook and not owner of MR
-            # Actually, yeah, see 'object_attributes'.'author_id'
-            # TODO: need to create getter for that
-            author_name=data['user']['name'],
-            author_url='https://gitlab.com/FIXME',
+            author_name=user.name,
+            author_url=user.web_url,
             repository_name=data['object_attributes']['target']['name'],
             repository_url=data['object_attributes']['target']['web_url'],
             pr_url=data['object_attributes']['url'],
